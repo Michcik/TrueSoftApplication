b346f32086401eee50b96d803219bc71b0396aa3
michcik <michciki007@gmail.com>
2026-03-05 23:30:25 +0000

    separate project

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,22 @@
+namespace ContactManager.Models
+{
+    public enum ContactCategory
+    {
+        Private,
+        Business,
+        Family
+    }
+
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public ContactCategory Category { get; set; }
+        public string AvatarPath { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}".Trim();
+    }
+}
